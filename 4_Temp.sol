996b658af15ac91754bf8ef1224b8ecfc33f8e3a
Kevinjsj <64388701+Kevinjsj@users.noreply.github.com>
2022-02-28 00:50:48 +0900

    Add files via upload

---
+++ 4_Temp.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+contract Temp {
+    string public data;
+    
+    function Temp() public {
+    }
+    
+    function enterData() public {
+        data = "test";
+    }
+
+    function enterData2() public {
+        data = "test2";
+    }
+}   
